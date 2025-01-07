47058dbb8c8893c9097634f6dca5e623d7cc6b37
darren2005234 <darrentouopi@gmail.com>
2025-01-07 14:59:20 +0100

    j'ajoute le ficheir produit.cpp

--- produit.cpp
+++ produit.cpp
@@ -6,3 +6,19 @@ Produit::Produit (std::string titre, std::string description, int pu, int qte): 
 Produit::Produit (std::string titre, std::string description, int pu): _titre(titre), _description(description), _pu(pu){
 
 }
+
+void ajouter_p(int qte){
+    _qte+= qte;
+}
+bool test_qte(int n){
+    return _qte>=n;
+}
+
+void diminuer_p(int qte){
+    if test_qte(qte){
+        _qte-= qte;
+    }
+    else{
+        std::cout<<"quantite insuffisane en stock"<<std::endl;
+    }
+}
